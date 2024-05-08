db4e3ebc18b64fef23a31b75f8e3544103cee59f
Aghmaz <sirsahil300@gmail.com>
2024-05-08 15:49:36 +0500

    react learning code ,functional component ,props , ,localstorage ,sessionstorage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Loop from "./Home";
+import Activity from "./Activity";
+import Problem1 from "./problem1";
+import Parent from "./Parent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Parent />
+
+      {/* <Loop /> */}
+      {/* <Problem1 /> */}
+    </>
   );
 }
 
